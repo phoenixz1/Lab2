917b6ee87c57e8e48002437a87d61134bd638d78
azharmu93 <azharmu93@users.noreply.github.com>
2015-02-12 14:53:09 -0500

    Added MW_INIT packet format

--- MazewarPacket.java
+++ MazewarPacket.java
@@ -10,7 +10,8 @@ public class MazewarPacket implements Serializable {
 	public static final int MW_REPLY       = 300;
 	public static final int MW_BYE         = 400;
 	public static final int MW_START       = 500;
-	public static final int MW_TICK		   = 600;
+	public static final int MW_TICK	       = 600;
+	public static final int MW_INIT	       = 700;
 	/* the packet payload */
 	
 	/* initialized to be a null packet */
