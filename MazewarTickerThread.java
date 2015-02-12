ba519e8cd44d238e89705fc142988ea4e7e807e5
phoenixz1 <anirudh.sanga@gmail.com>
2015-02-12 08:42:32 -0500

    clarification required

--- MazewarTickerThread.java
+++ MazewarTickerThread.java
@@ -5,11 +5,13 @@ public class MazewarTickerThread()extends Thread{
 	public MazewarTickerThread(){
 		super("MazewarServerHandlerThread");
 	}
-	
+	// might need a while loop in run()
+	// TickerThread should only be created on receiving a 'fire' packet ?
 	public void run(){
 		MazewarPacket tik =  new MazewarPacket();
 		tik.type = MW_TICK;
 		broadcast(tik); //can multiple broadcast cause problems? plz confirm
+		// no, but there isnt a broadcast function in this class
 		try{
 			Thread.sleep(200);
 		}catch (InterruptedException e) {
